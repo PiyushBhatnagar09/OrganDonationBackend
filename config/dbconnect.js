01d36933920950381a832501b0fe2759cb7fdfc2
Piyush Bhatnagar <113785116+PiyushBhatnagar09@users.noreply.github.com>
2024-07-14 22:39:53 +0530

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,7 +1,7 @@
 //connecting to database using mongoose
 const mongoose= require('mongoose');
 
-mongoose.connect(`mongodb+srv://pbpiyush34:piyushbhat@cluster0.sreyp4a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+mongoose.connect(`mongodb+srv://pbpiyush34:piyushbhat@cluster0.sreyp4a.mongodb.net/data?retryWrites=true&w=majority&appName=Cluster0`);
 // mongoose.connect(`mongodb://localhost:27017/OrganDonation`);
 
 //making connection with database
